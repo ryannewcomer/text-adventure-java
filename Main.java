2c775988199b70b29224b44301b3b12c960cc8fb
Ryan Hung <ryanhungryan@gmail.com>
2024-06-15 21:22:04 +0100

    feat(main.java): creat introduction

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    // System.out.println("Hello World!");
+    System.out.println("Hello!");
+    System.out.println("Welcome to the text adventage game!");
+
+    System.out.println("This is the story about you, who just decover a time-travel device deep underground.");
+
   }
 }
